b8979d40f4bd4482243b0ca8b08a9d225bc477d6
Matthew Kern <mattcs321@gmail.com>
2024-12-22 14:49:48 -0500

    move to end

---
+++ src/arrays/moveallzerostoend.java
@@ -0,0 +1,22 @@
+package arrays;
+
+//move all 0s to end
+public class moveallzerostoend {
+ 
+	
+	static int[] movetoend() {
+		
+		int[] temp= {};
+		
+		return temp;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int[] arr= {1,2,0,4,3,0,5,0};
+		
+		System.out.println("");
+	}
+	
+}
